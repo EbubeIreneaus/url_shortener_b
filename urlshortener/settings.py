5855b61640c0d6bc4e55861f461a61a6d165891b
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2024-01-05 16:40:47 +0100

    1000001

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -132,7 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'qr_codes/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'qrcodes/')
 # Default primary key field type
